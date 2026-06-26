d101699f01201bc680ead293e470a67ac3a92d7f
Marine Team <mightyteamvcs@gmail.com>
2026-06-26 16:45:03 -0700

    Remove unnecessary blank line in viewers.js

--- pages/api/admin/viewers.js
+++ pages/api/admin/viewers.js
@@ -2,7 +2,6 @@ import { getSession } from '@auth0/nextjs-auth0';
 import { redis } from '../../../lib/redis';
 import { isAdmin } from '../../../lib/auth';
 
-
 export default async function handler(req, res) {
   const session = await getSession(req, res);
   if (!session || !isAdmin(session?.user?.email)) return res.status(403).json({ error: 'Forbidden' });
